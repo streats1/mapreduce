1258028c72bb55cd8ed9651ec8f401e83fefa1a6
streats <streats99@gmail.com>
2017-03-23 16:01:47 +0900

    zzeqweqwe

--- src/main/java/com/bit2017/mapreduce/trigram/Trigram.java
+++ src/main/java/com/bit2017/mapreduce/trigram/Trigram.java
@@ -3,8 +3,6 @@ package com.bit2017.mapreduce.trigram;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
